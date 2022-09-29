a9f6a33ad4214d465205c7239fe96b76aced3d4e
muaxz <bexsd@hotmail.com>
2022-09-28 23:17:21 -0400

    change

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const PORT = 3001 || process.env.PORT
+
+app.use(express.static("public"))
+
+app.get("*",(req,res,next)=>{
+  res.sendFile(path.resolve(__dirname,"../public","index.html"))
+  //res.sendFile(path.resolve(__dirname,"public","index.html"))
+})
+
+
+app.listen(PORT,()=>{console.log("listening...")})
