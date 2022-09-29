037c182353f837575058be30f7b9ff40ce34f88d
muaxz <bexsd@hotmail.com>
2022-09-28 23:25:23 -0400

    change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const path = require("path")
 const app = express()
-const PORT = 3001 || process.env.PORT
+const PORT = process.env.PORT || 3001
 
 app.use(express.static("public"))
 
